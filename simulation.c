9a595fc617aba21719ae06778967fdbf54ab5c7b
flyme2bluemoon <37197710+flyme2bluemoon@users.noreply.github.com>
2020-12-26 17:08:21 -0500

    Added for statistics to the summary page

--- simulation.c
+++ simulation.c
@@ -151,6 +151,40 @@ void virus_transmission(int transmission_distance, int width, int population, in
     return;
 }
 
+double calculate_number_of_infections(int population, Person people[population]) {
+    double infected = 0;
+    for (int i = 0; i < population; i++) {
+        if (people[i].status != -1) {
+            infected++;
+        }
+    }
+
+    return infected;
+}
+
+double calculate_infected_percentage(int population, Person people[population]) {
+    double infected = calculate_number_of_infections(population, people);
+
+    double percentage = (infected / population) * 100;
+
+    return percentage;
+}
+
+int add_new_cases(int population, Person people[population], int current_day, int days, int case_count, int daily_cases[days]) {
+    double infected = calculate_number_of_infections(population, people);
+    int new_cases = (int)infected - case_count;
+
+    daily_cases[current_day] = new_cases;
+
+    return new_cases;
+}
+
+void print_daily_case_counts(int days, int daily_cases[days]) {
+    for (int i = 0; i < days; i++) {
+        printf("Day %d: %d cases\n", i + 1, daily_cases[i]);
+    }
+}
+
 int main(void) {
     // print greeting message
     greet();
@@ -166,12 +200,18 @@ int main(void) {
     int transmission_distance = get_int("Range of virus transmission (recommended: 1):");
     int recovery_time = get_int("Recovery time (recommended: 14):");
 
+    // init case count to zero
+    int case_count = 0;
+    int daily_cases[days];
+
     /*
         Test input:
         width=25
         population=5
         days=5
         range=2
+        transmission_distance=1
+        recovery_time=14
     */
 
     if (population > width * width) {
@@ -179,6 +219,7 @@ int main(void) {
         return 1;
     } else if (range > width) {
         printf("Range must be less than the width.\n");
+        return 1;
     }
 
     printf("\nRunning Simulation!\n\n");
@@ -207,24 +248,34 @@ int main(void) {
     // patient zero
     int patient_zero = rand() % population;
     people[patient_zero].status = 1;
+    case_count++;
 
     // actual code
     for (int i = 1; i <= days; i++) {
-        printf("[*] Day %d\n==========\n", i);
+        printf("\n[*] Day %d\n==========\n", i);
         printf("Incrementing...\n");
         increment_day(population, people, recovery_time);
         printf("Moving...\n");
         move_people(range, width, population, map, people);
         printf("Simulating virus transmission...\n");
         virus_transmission(transmission_distance, width, population, map, people);
+        printf("Tallying case counts...\n");
+        int new_cases = add_new_cases(population, people, i - 1, days, case_count, daily_cases);
+        case_count += new_cases;
+        printf("Daily case count: %d\n", new_cases);
         printf("Printing map...\n");
         print_map(width, population, map, people);
-        printf("Printing Status...\n");
-        print_status(population, people);
+        // printf("Printing Status...\n");
+        // print_status(population, people);
     }
 
+    // Prints summary
     printf("\n====SUMMARY====\n");
     printf("Patient Zero: %d\n", patient_zero);
+    double percentage_infected = calculate_infected_percentage(population, people);
+    printf("Percentage infected: %.2f%%\n", percentage_infected);
+    printf("Total number of cases: %d\n", case_count);
+    print_daily_case_counts(days, daily_cases);
 
     return 0;
 }
