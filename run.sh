cebfbb11f49a11119ed5e76f3a14852377b994d0
flyme2bluemoon <37197710+flyme2bluemoon@users.noreply.github.com>
2020-12-26 14:03:27 -0500

    Updated run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ width=25;
 population=5;
 days=5;
 range=2;
+recovery_time=14;
 
 make simulation
 
-echo "$width\n$population\n$days\n$range\n" | ./simulation
+echo "$width\n$population\n$days\n$range\n$recovery_time\n" | ./simulation
